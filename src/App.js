025bea13a997035b18aef53fa8811ed58506792a
harryg15 <harry.grant99@googlemail.com>
2023-04-05 11:53:46 +0100

    added mapping of pokedex data & displaying a card for each pokemon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import { InputGroup, Form, Container, Row, Col } from 'react-bootstrap';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
+  const [pokemonList, setPokemonList] = useState([])
+  const [filterText, setFilterText] = useState("")
+  console.log(pokemonList)
+
+  useEffect(() => {
+    const fetching = async () => {
+      const response = await fetch(pokeApi)
+      const data = await response.json()
+      setPokemonList(data.results)
+    }
+    fetching()
+  }, [])
+
+  const handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      const result = pokemonList.filter(pokemon => {
+        return 
+      })
+    } else {
+
+    }
+}
+
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Pokemon"
+          aria-label="Pokemon"
+          aria-describedby="basic-addon1"
+          onChange={handleChange}
+        />
+      </InputGroup>
+      <Container className='col-md-8'>
+      <Row>
+      {pokemonList.map(pokemon => <PokemonCard name={pokemon.name} url={pokemon.url}/> )}
+      </Row>
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { Card, Row, Col } from "react-bootstrap"
 
 function PokemonCard({ url, name }) {
 
-  return (
+  const [pokeURL, setPokeURL] = useState("")
+  console.log(pokeURL)
+
+  useEffect(() => {
+    const fetching = async () => {
+      const response = await fetch(url)
+      const data = await response.json()
+      setPokeURL(data)
+    }
+    fetching()
+  
+  }, [])
+  
+
+  return (<>
+    {pokeURL ?
+    <Col>
+      <Card>
+      <Card.Img src={pokeURL.sprites.front_default} />
+      <Card.Title>{name}</Card.Title>
+      <Card.Text>
+        <ul>
+          {pokeURL.abilities.map((ability) => <li>{ability.ability.name}</li>)}
+        </ul>
+      </Card.Text>
+      </Card>
+      </Col> : 
     <div>
-        pokemon card
+      Loading...
     </div>
-  );
+    }
+    </>);
 }
 
 export { PokemonCard };
